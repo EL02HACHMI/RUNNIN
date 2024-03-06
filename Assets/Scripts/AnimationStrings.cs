b72683cac66b73b9010f841d7a1f860ed80dd2c6
El_Hachmi <88097240+EL02HACHMI@users.noreply.github.com>
2024-03-05 21:45:10 -0800

    Tilemap Added

---
+++ Assets/Scripts/AnimationStrings.cs
@@ -0,0 +1,13 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System.Threading.Tasks;
+using System.Text;
+using System.Linq;
+
+
+internal class AnimationStrings
+{
+	internal static string isMoving = "isMoving";
+	internal static string isRunning = "isRunning";
+}
--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -13,6 +13,78 @@ public class PlayerController : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
+<<<<<<< Updated upstream
         
+=======
+        get => _isMoving;
+        private set
+        {
+            _isMoving = value;
+            animator.SetBool(AnimationStrings.isMoving, value);
+        }
+    }
+
+    [SerializeField]
+    private bool _isRunning = false;
+    public bool IsRunning
+    {
+        get => _isRunning;
+        set
+        {
+            _isRunning = value;
+            animator.SetBool(AnimationStrings.isRunning, value);
+        }
+    }
+
+    public bool IsFacingRight
+    {
+        get => _isFacingRight;
+        private set
+        {
+            if (_isFacingRight != value)
+            {
+                _isFacingRight = value;
+                transform.localScale = new Vector3(-transform.localScale.x, 1, 1); // Corrected the Vector constructor
+            }
+        }
+    }
+
+    private Rigidbody2D rb;
+    private Animator animator;
+
+    private void Awake()
+    {
+        rb = GetComponent<Rigidbody2D>();
+        animator = GetComponent<Animator>();
+    }
+
+    private void FixedUpdate()
+    {
+        rb.velocity = new Vector2(moveInput.x * CurrentMoveSpeed, rb.velocity.y);
+    }
+
+    public void OnMove(InputAction.CallbackContext context)
+    {
+        moveInput = context.ReadValue<Vector2>();
+        IsMoving = moveInput != Vector2.zero;
+        SetFacingDirection(moveInput); // Moved SetFacingDirection call to here with parameter
+    }
+
+    private void SetFacingDirection(Vector2 moveInput)
+    {
+        if (moveInput.x > 0 && !_isFacingRight) // Corrected the condition check
+        {
+            IsFacingRight = true;
+        }
+        else if (moveInput.x < 0 && _isFacingRight) // Corrected the condition check
+        {
+            IsFacingRight = false;
+        }
+    }
+
+    public void OnRun(InputAction.CallbackContext context)
+    {
+        IsRunning = context.ReadValueAsButton();
+>>>>>>> Stashed changes
     }
 }
