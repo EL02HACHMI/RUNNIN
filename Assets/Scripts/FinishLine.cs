bf9d8f22456d3f08ea451a78c229b710cb903504
OusSka <oussamaskalli1@gmail.com>
2024-04-03 11:28:04 +0000

    last version

--- Assets/Scripts/FinishLine.cs
+++ Assets/Scripts/FinishLine.cs
@@ -4,18 +4,25 @@ using System.Collections;
 using TMPro;
 using Photon.Pun;
 using Unity.VisualScripting;
+using System.Collections.Generic;
+using UnityEngine.SocialPlatforms.Impl;
 
 public class FinishLine : MonoBehaviourPun
 {
     public TMP_Text winnerText; // Assign in the Inspector
     public TMP_Text timerText; // Assign in the Inspector
     private bool winnerDeclared = false;
-    private float countdown = 1f; // 2 minutes countdown
+    private float countdown = 15f; // 2 minutes countdown
     private bool startCountdown = false;
-    private bool raceFinished = false;
+    private float TimerUp = 0f;
+    private int score = 0;
+    public GameObject playerCamera;
+    private Dictionary<int, float> playerTimes = new Dictionary<int, float>(); // Stocker le temps écoulé pour chaque joueur
 
     private void Update()
     {
+            // Incrémenter le temps écoulé
+            TimerUp += Time.deltaTime;
         if (startCountdown && countdown >= 0)
         {
         // Obtenez le composant PhotonView de l'objet joueur
@@ -40,7 +47,7 @@ public class FinishLine : MonoBehaviourPun
 
     private void OnTriggerEnter2D(Collider2D other)
     {
-        if (!winnerDeclared && other.gameObject.CompareTag("Player"))
+        if (other.gameObject.CompareTag("Player"))
         {
             winnerDeclared = true;
             startCountdown = true;
@@ -49,14 +56,33 @@ public class FinishLine : MonoBehaviourPun
 
                 // Obtenez le pseudo du joueur propriétaire du PhotonView
                 string playerName = pv.Owner.NickName;
-            // pv.gameObject.GetComponentInChildren<TMP_Text>().text = playerName + " is the winner!";
-                //  pv.gameObject.GetComponentInChildren<TMP_Text>().gameObject.SetActive(true);
+                //pv.gameObject.GetComponentInChildren<TMP_Text>().text = playerName + " is the winner!";
+                //pv.gameObject.GetComponentInChildren<TMP_Text>().gameObject.SetActive(true);
                 winnerText.text = playerName + " is the winner!";
                 winnerText.gameObject.SetActive(true);
-                   
+                // Stocker le temps écoulé pour ce joueur
+                playerTimes[pv.ViewID] = TimerUp;
+                int playerScore = CalculateScore(TimerUp); // Calculer le score pour ce joueur
+                string playerScoreKey = "PlayerScore_" + pv.Owner.ActorNumber;
+                PlayerPrefs.SetInt(playerScoreKey, playerScore); // Stocker le score dans PlayerPrefs
+                // Désactiver la caméra uniquement pour le joueur gagnant
+
+                    Debug.Log(playerCamera);
+                playerCamera.SetActive(false);
+                
         }
     }
-
+    private int CalculateScore(float playerTime)
+    {
+        // Vous pouvez ajuster la logique de calcul du score selon vos besoins
+        // Par exemple, attribuer un score plus élevé à un temps plus court
+            if (playerTime != 0f)
+    {
+            score = 1000 / (Mathf.RoundToInt(playerTime)); // Exemple de logique de score
+        Debug.Log("Player's Score: " + score);
+    }
+    return score;
+    }
     private void EndGame()
     {
         // Logic to end the game if timer runs out
---
+++ Assets/Scripts/Results.cs
@@ -0,0 +1,41 @@
+using UnityEngine;
+using UnityEngine.UI;
+using TMPro;
+using Photon.Pun;
+using Photon.Realtime;
+using System.Linq;
+
+public class Results : MonoBehaviour
+{
+    public GameObject resultPrefab; // Prefab de l'objet container
+    public Transform resultsContainer; // Parent où les résultats seront affichés
+
+    private void Start()
+    {
+        // Récupérer le nombre de joueurs et afficher les résultats pour chaque joueur
+        int playerCount = PhotonNetwork.CurrentRoom.PlayerCount;
+        float templateHeight = 1.2f;
+        // Trier les joueurs par score décroissant
+        Player[] sortedPlayers = PhotonNetwork.PlayerList.OrderByDescending(player =>
+            PlayerPrefs.GetInt("PlayerScore_" + player.ActorNumber)).ToArray();
+        for (int i = 0; i < PhotonNetwork.PlayerList.Length; i++)
+        {
+            // Créer une copie de l'objet container
+            GameObject resultObject = Instantiate(resultPrefab, resultsContainer);
+            RectTransform entryRectTransform = resultObject.GetComponent<RectTransform>();
+            entryRectTransform.anchoredPosition = new Vector2(0, -templateHeight * i);
+            Player player = sortedPlayers[i];
+            //Player player = PhotonNetwork.PlayerList[i];
+            string playerName = player.NickName;
+            int position = i+1;
+            int playerScore = PlayerPrefs.GetInt("PlayerScore_" + player.ActorNumber);
+
+            //int score = PlayerPrefs.GetInt("PlayerScore");
+
+            // Mettre à jour les textes de l'objet container avec les données du joueur
+            resultObject.transform.Find("PositionText").GetComponent<TextMeshProUGUI>().text = position.ToString();
+            resultObject.transform.Find("PlayerNameText").GetComponent<TextMeshProUGUI>().text = playerName;
+            resultObject.transform.Find("ScoreText").GetComponent<TextMeshProUGUI>().text = playerScore.ToString();
+        }
+    }
+}
